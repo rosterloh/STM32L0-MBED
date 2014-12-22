7dd50915b828473327d2a1e301ae327287c882ce
Richard Osterloh <richard.osterloh@gmail.com>
2014-12-22 14:09:15 +0000

    Current testing and development

---
+++ projects/USBHostHub/build/KL25Z/main.d
@@ -0,0 +1,273 @@
+build/KL25Z/main.o: main.cpp ../../mbed-src/libraries/mbed/api/mbed.h \
+ ../../mbed-src/libraries/mbed/api/platform.h \
+ ../../mbed-src/libraries/mbed/targets/hal/TARGET_Freescale/TARGET_KLXX/TARGET_KL25Z/device.h \
+ ../../mbed-src/libraries/mbed/targets/hal/TARGET_Freescale/TARGET_KLXX/objects.h \
+ ../../mbed-src/libraries/mbed/targets/cmsis/TARGET_Freescale/TARGET_KLXX/TARGET_KL25Z/cmsis.h \
+ ../../mbed-src/libraries/mbed/targets/cmsis/TARGET_Freescale/TARGET_KLXX/TARGET_KL25Z/MKL25Z4.h \
+ ../../mbed-src/libraries/mbed/targets/cmsis/core_cm0plus.h \
+ ../../mbed-src/libraries/mbed/targets/cmsis/core_cmInstr.h \
+ ../../mbed-src/libraries/mbed/targets/cmsis/core_cmFunc.h \
+ ../../mbed-src/libraries/mbed/targets/cmsis/TARGET_Freescale/TARGET_KLXX/TARGET_KL25Z/system_MKL25Z4.h \
+ ../../mbed-src/libraries/mbed/targets/cmsis/TARGET_Freescale/TARGET_KLXX/TARGET_KL25Z/cmsis_nvic.h \
+ ../../mbed-src/libraries/mbed/targets/cmsis/TARGET_Freescale/TARGET_KLXX/TARGET_KL25Z/cmsis.h \
+ ../../mbed-src/libraries/mbed/targets/hal/TARGET_Freescale/TARGET_KLXX/PortNames.h \
+ ../../mbed-src/libraries/mbed/targets/hal/TARGET_Freescale/TARGET_KLXX/TARGET_KL25Z/PeripheralNames.h \
+ ../../mbed-src/libraries/mbed/targets/hal/TARGET_Freescale/TARGET_KLXX/TARGET_KL25Z/PinNames.h \
+ ../../mbed-src/libraries/mbed/targets/hal/TARGET_Freescale/TARGET_KLXX/gpio_object.h \
+ ../../mbed-src/libraries/mbed/api/mbed_assert.h \
+ ../../mbed-src/libraries/mbed/api/mbed_error.h \
+ ../../mbed-src/libraries/mbed/api/mbed_interface.h \
+ ../../mbed-src/libraries/mbed/api/DigitalIn.h \
+ ../../mbed-src/libraries/mbed/hal/gpio_api.h \
+ ../../mbed-src/libraries/mbed/api/DigitalOut.h \
+ ../../mbed-src/libraries/mbed/api/DigitalInOut.h \
+ ../../mbed-src/libraries/mbed/api/BusIn.h \
+ ../../mbed-src/libraries/mbed/api/BusOut.h \
+ ../../mbed-src/libraries/mbed/api/BusInOut.h \
+ ../../mbed-src/libraries/mbed/api/PortIn.h \
+ ../../mbed-src/libraries/mbed/hal/port_api.h \
+ ../../mbed-src/libraries/mbed/api/PortInOut.h \
+ ../../mbed-src/libraries/mbed/api/PortOut.h \
+ ../../mbed-src/libraries/mbed/api/AnalogIn.h \
+ ../../mbed-src/libraries/mbed/hal/analogin_api.h \
+ ../../mbed-src/libraries/mbed/api/AnalogOut.h \
+ ../../mbed-src/libraries/mbed/hal/analogout_api.h \
+ ../../mbed-src/libraries/mbed/api/PwmOut.h \
+ ../../mbed-src/libraries/mbed/hal/pwmout_api.h \
+ ../../mbed-src/libraries/mbed/api/Serial.h \
+ ../../mbed-src/libraries/mbed/api/Stream.h \
+ ../../mbed-src/libraries/mbed/api/FileLike.h \
+ ../../mbed-src/libraries/mbed/api/FileBase.h \
+ ../../mbed-src/libraries/mbed/api/FileHandle.h \
+ ../../mbed-src/libraries/mbed/api/SerialBase.h \
+ ../../mbed-src/libraries/mbed/api/FunctionPointer.h \
+ ../../mbed-src/libraries/mbed/hal/serial_api.h \
+ ../../mbed-src/libraries/mbed/api/SPI.h \
+ ../../mbed-src/libraries/mbed/hal/spi_api.h \
+ ../../mbed-src/libraries/mbed/api/SPISlave.h \
+ ../../mbed-src/libraries/mbed/api/I2C.h \
+ ../../mbed-src/libraries/mbed/hal/i2c_api.h \
+ ../../mbed-src/libraries/mbed/api/I2CSlave.h \
+ ../../mbed-src/libraries/mbed/api/Ethernet.h \
+ ../../mbed-src/libraries/mbed/api/CAN.h \
+ ../../mbed-src/libraries/mbed/api/RawSerial.h \
+ ../../mbed-src/libraries/mbed/api/Timer.h \
+ ../../mbed-src/libraries/mbed/api/Ticker.h \
+ ../../mbed-src/libraries/mbed/api/TimerEvent.h \
+ ../../mbed-src/libraries/mbed/hal/us_ticker_api.h \
+ ../../mbed-src/libraries/mbed/api/Timeout.h \
+ ../../mbed-src/libraries/mbed/api/LocalFileSystem.h \
+ ../../mbed-src/libraries/mbed/api/InterruptIn.h \
+ ../../mbed-src/libraries/mbed/hal/gpio_irq_api.h \
+ ../../mbed-src/libraries/mbed/api/wait_api.h \
+ ../../mbed-src/libraries/mbed/hal/sleep_api.h \
+ ../../mbed-src/libraries/mbed/api/rtc_time.h \
+ ../../mbed-src/libraries/USBHost/USBHostHID/USBHostKeyboard.h \
+ ../../mbed-src/libraries/USBHost/USBHost/USBHostConf.h \
+ ../../mbed-src/libraries/USBHost/USBHost/USBHost.h \
+ ../../mbed-src/libraries/USBHost/USBHost/USBHALHost.h \
+ ../../mbed-src/libraries/USBHost/USBHost/USBHostTypes.h \
+ ../../mbed-src/libraries/mbed/api/toolchain.h \
+ ../../mbed-src/libraries/USBHost/USBHost/USBHostConf.h \
+ ../../mbed-src/libraries/USBHost/USBHost/USBDeviceConnected.h \
+ ../../mbed-src/libraries/USBHost/USBHost/USBEndpoint.h \
+ ../../mbed-src/libraries/mbed/api/FunctionPointer.h \
+ ../../mbed-src/libraries/rtos/rtos/rtos.h \
+ ../../mbed-src/libraries/rtos/rtos/Thread.h \
+ ../../mbed-src/libraries/rtos/rtx/TARGET_CORTEX_M/cmsis_os.h \
+ ../../mbed-src/libraries/rtos/rtx/TARGET_CORTEX_M/os_tcb.h \
+ ../../mbed-src/libraries/rtos/rtos/Mutex.h \
+ ../../mbed-src/libraries/rtos/rtos/RtosTimer.h \
+ ../../mbed-src/libraries/rtos/rtos/Semaphore.h \
+ ../../mbed-src/libraries/rtos/rtos/Mail.h \
+ ../../mbed-src/libraries/rtos/rtos/MemoryPool.h \
+ ../../mbed-src/libraries/rtos/rtos/Queue.h \
+ ../../mbed-src/libraries/mbed/api/mbed_error.h \
+ ../../mbed-src/libraries/USBHost/USBHost/IUSBEnumerator.h \
+ ../../mbed-src/libraries/USBHost/USBHost/dbg.h \
+ ../../mbed-src/libraries/USBHost/USBHostHub/USBHostHub.h \
+ ../../mbed-src/libraries/USBHost/USBHost/USBHostTypes.h \
+ ../../mbed-src/libraries/USBHost/USBHost/IUSBEnumerator.h \
+ ../../mbed-src/libraries/USBHost/USBHostHID/USBHostMouse.h
+
+../../mbed-src/libraries/mbed/api/mbed.h:
+
+../../mbed-src/libraries/mbed/api/platform.h:
+
+../../mbed-src/libraries/mbed/targets/hal/TARGET_Freescale/TARGET_KLXX/TARGET_KL25Z/device.h:
+
+../../mbed-src/libraries/mbed/targets/hal/TARGET_Freescale/TARGET_KLXX/objects.h:
+
+../../mbed-src/libraries/mbed/targets/cmsis/TARGET_Freescale/TARGET_KLXX/TARGET_KL25Z/cmsis.h:
+
+../../mbed-src/libraries/mbed/targets/cmsis/TARGET_Freescale/TARGET_KLXX/TARGET_KL25Z/MKL25Z4.h:
+
+../../mbed-src/libraries/mbed/targets/cmsis/core_cm0plus.h:
+
+../../mbed-src/libraries/mbed/targets/cmsis/core_cmInstr.h:
+
+../../mbed-src/libraries/mbed/targets/cmsis/core_cmFunc.h:
+
+../../mbed-src/libraries/mbed/targets/cmsis/TARGET_Freescale/TARGET_KLXX/TARGET_KL25Z/system_MKL25Z4.h:
+
+../../mbed-src/libraries/mbed/targets/cmsis/TARGET_Freescale/TARGET_KLXX/TARGET_KL25Z/cmsis_nvic.h:
+
+../../mbed-src/libraries/mbed/targets/cmsis/TARGET_Freescale/TARGET_KLXX/TARGET_KL25Z/cmsis.h:
+
+../../mbed-src/libraries/mbed/targets/hal/TARGET_Freescale/TARGET_KLXX/PortNames.h:
+
+../../mbed-src/libraries/mbed/targets/hal/TARGET_Freescale/TARGET_KLXX/TARGET_KL25Z/PeripheralNames.h:
+
+../../mbed-src/libraries/mbed/targets/hal/TARGET_Freescale/TARGET_KLXX/TARGET_KL25Z/PinNames.h:
+
+../../mbed-src/libraries/mbed/targets/hal/TARGET_Freescale/TARGET_KLXX/gpio_object.h:
+
+../../mbed-src/libraries/mbed/api/mbed_assert.h:
+
+../../mbed-src/libraries/mbed/api/mbed_error.h:
+
+../../mbed-src/libraries/mbed/api/mbed_interface.h:
+
+../../mbed-src/libraries/mbed/api/DigitalIn.h:
+
+../../mbed-src/libraries/mbed/hal/gpio_api.h:
+
+../../mbed-src/libraries/mbed/api/DigitalOut.h:
+
+../../mbed-src/libraries/mbed/api/DigitalInOut.h:
+
+../../mbed-src/libraries/mbed/api/BusIn.h:
+
+../../mbed-src/libraries/mbed/api/BusOut.h:
+
+../../mbed-src/libraries/mbed/api/BusInOut.h:
+
+../../mbed-src/libraries/mbed/api/PortIn.h:
+
+../../mbed-src/libraries/mbed/hal/port_api.h:
+
+../../mbed-src/libraries/mbed/api/PortInOut.h:
+
+../../mbed-src/libraries/mbed/api/PortOut.h:
+
+../../mbed-src/libraries/mbed/api/AnalogIn.h:
+
+../../mbed-src/libraries/mbed/hal/analogin_api.h:
+
+../../mbed-src/libraries/mbed/api/AnalogOut.h:
+
+../../mbed-src/libraries/mbed/hal/analogout_api.h:
+
+../../mbed-src/libraries/mbed/api/PwmOut.h:
+
+../../mbed-src/libraries/mbed/hal/pwmout_api.h:
+
+../../mbed-src/libraries/mbed/api/Serial.h:
+
+../../mbed-src/libraries/mbed/api/Stream.h:
+
+../../mbed-src/libraries/mbed/api/FileLike.h:
+
+../../mbed-src/libraries/mbed/api/FileBase.h:
+
+../../mbed-src/libraries/mbed/api/FileHandle.h:
+
+../../mbed-src/libraries/mbed/api/SerialBase.h:
+
+../../mbed-src/libraries/mbed/api/FunctionPointer.h:
+
+../../mbed-src/libraries/mbed/hal/serial_api.h:
+
+../../mbed-src/libraries/mbed/api/SPI.h:
+
+../../mbed-src/libraries/mbed/hal/spi_api.h:
+
+../../mbed-src/libraries/mbed/api/SPISlave.h:
+
+../../mbed-src/libraries/mbed/api/I2C.h:
+
+../../mbed-src/libraries/mbed/hal/i2c_api.h:
+
+../../mbed-src/libraries/mbed/api/I2CSlave.h:
+
+../../mbed-src/libraries/mbed/api/Ethernet.h:
+
+../../mbed-src/libraries/mbed/api/CAN.h:
+
+../../mbed-src/libraries/mbed/api/RawSerial.h:
+
+../../mbed-src/libraries/mbed/api/Timer.h:
+
+../../mbed-src/libraries/mbed/api/Ticker.h:
+
+../../mbed-src/libraries/mbed/api/TimerEvent.h:
+
+../../mbed-src/libraries/mbed/hal/us_ticker_api.h:
+
+../../mbed-src/libraries/mbed/api/Timeout.h:
+
+../../mbed-src/libraries/mbed/api/LocalFileSystem.h:
+
+../../mbed-src/libraries/mbed/api/InterruptIn.h:
+
+../../mbed-src/libraries/mbed/hal/gpio_irq_api.h:
+
+../../mbed-src/libraries/mbed/api/wait_api.h:
+
+../../mbed-src/libraries/mbed/hal/sleep_api.h:
+
+../../mbed-src/libraries/mbed/api/rtc_time.h:
+
+../../mbed-src/libraries/USBHost/USBHostHID/USBHostKeyboard.h:
+
+../../mbed-src/libraries/USBHost/USBHost/USBHostConf.h:
+
+../../mbed-src/libraries/USBHost/USBHost/USBHost.h:
+
+../../mbed-src/libraries/USBHost/USBHost/USBHALHost.h:
+
+../../mbed-src/libraries/USBHost/USBHost/USBHostTypes.h:
+
+../../mbed-src/libraries/mbed/api/toolchain.h:
+
+../../mbed-src/libraries/USBHost/USBHost/USBHostConf.h:
+
+../../mbed-src/libraries/USBHost/USBHost/USBDeviceConnected.h:
+
+../../mbed-src/libraries/USBHost/USBHost/USBEndpoint.h:
+
+../../mbed-src/libraries/mbed/api/FunctionPointer.h:
+
+../../mbed-src/libraries/rtos/rtos/rtos.h:
+
+../../mbed-src/libraries/rtos/rtos/Thread.h:
+
+../../mbed-src/libraries/rtos/rtx/TARGET_CORTEX_M/cmsis_os.h:
+
+../../mbed-src/libraries/rtos/rtx/TARGET_CORTEX_M/os_tcb.h:
+
+../../mbed-src/libraries/rtos/rtos/Mutex.h:
+
+../../mbed-src/libraries/rtos/rtos/RtosTimer.h:
+
+../../mbed-src/libraries/rtos/rtos/Semaphore.h:
+
+../../mbed-src/libraries/rtos/rtos/Mail.h:
+
+../../mbed-src/libraries/rtos/rtos/MemoryPool.h:
+
+../../mbed-src/libraries/rtos/rtos/Queue.h:
+
+../../mbed-src/libraries/mbed/api/mbed_error.h:
+
+../../mbed-src/libraries/USBHost/USBHost/IUSBEnumerator.h:
+
+../../mbed-src/libraries/USBHost/USBHost/dbg.h:
+
+../../mbed-src/libraries/USBHost/USBHostHub/USBHostHub.h:
+
+../../mbed-src/libraries/USBHost/USBHost/USBHostTypes.h:
+
+../../mbed-src/libraries/USBHost/USBHost/IUSBEnumerator.h:
+
+../../mbed-src/libraries/USBHost/USBHostHID/USBHostMouse.h:
